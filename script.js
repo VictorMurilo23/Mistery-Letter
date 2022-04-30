52ee595a83361514681f49d8a4dc90c7a4d311cb
Victor Murilo <victor2706.vm@gmail.com>
2022-04-30 16:44:03 -0300

    Cumpri o requisito 5

--- script.js
+++ script.js
@@ -2,13 +2,16 @@ const textoCarta = document.querySelector('#carta-texto');
 const botaoGerar = document.querySelector('#criar-carta');
 const carta = document.querySelector('#carta-gerada');
 
-botaoGerar.addEventListener('click', (event) => {
-  event.preventDefault();
+botaoGerar.addEventListener('click', () => {
   carta.innerText = ' ';
   const palavras = textoCarta.value.split(' ');
-  for (let index = 0; index < palavras.length; index += 1) {
-    const span = document.createElement('span');
-    span.innerText = palavras[index];
-    carta.appendChild(span);
+  if (palavras[0] !== '') {
+    for (let index = 0; index < palavras.length; index += 1) {
+      const span = document.createElement('span');
+      span.innerText = palavras[index];
+      carta.appendChild(span);
+    }
+  } else {
+    carta.innerText = 'Por favor, digite o conteúdo da carta.';
   }
 });
