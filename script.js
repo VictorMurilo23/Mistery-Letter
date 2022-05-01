662354bce2cd961f7fa51fee59fb3770a60f772b
Victor Murilo <victor2706.vm@gmail.com>
2022-05-01 12:34:21 -0300

    Cumpri o requisito 18

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const textoCarta = document.querySelector('#carta-texto');
 const botaoGerar = document.querySelector('#criar-carta');
 const carta = document.querySelector('#carta-gerada');
+const contador = document.querySelector('#carta-contador');
 const estilo = ['newspaper', 'magazine1', 'magazine2'];
 const tamanho = ['medium', 'big', 'reallybig'];
 const rotacao = ['rotateleft', 'rotateright'];
@@ -9,7 +10,6 @@ const inclinacao = ['skewleft', 'skewright'];
 botaoGerar.addEventListener('click', () => {
   carta.innerText = ' ';
   const palavras = textoCarta.value.split(' ');
-
   if (palavras[0] !== '') {
     for (let index = 0; index < palavras.length; index += 1) {
       const span = document.createElement('span');
@@ -20,6 +20,7 @@ botaoGerar.addEventListener('click', () => {
       span.innerText = palavras[index];
       carta.appendChild(span);
     }
+    contador.innerText = palavras.length;
   } else {
     carta.innerText = 'Por favor, digite o conteúdo da carta.';
   }
